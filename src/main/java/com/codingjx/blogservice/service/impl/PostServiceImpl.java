9d825831019187e0f310ff06fe2f69b29ce6031d
chiahsingchu <chiahsingtony@gmail.com>
2023-08-25 09:56:53 -0700

    Implement update post by id api

--- src/main/java/com/codingjx/blogservice/service/impl/PostServiceImpl.java
+++ src/main/java/com/codingjx/blogservice/service/impl/PostServiceImpl.java
@@ -42,6 +42,19 @@ public class PostServiceImpl implements PostService {
         return mapToDto(post);
     }
 
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post updatedPost = postRepository.save(post);
+        return mapToDto(updatedPost);
+    }
+
+
     private Post mapToEntity(PostDto postDto) {
         Post post = new Post();
         post.setTitle(postDto.getTitle());
