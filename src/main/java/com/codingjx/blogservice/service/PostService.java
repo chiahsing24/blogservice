ee2fd63d98e29562381e32472615dbf5e5edb31d
chiahsingchu <chiahsingtony@gmail.com>
2023-08-25 09:37:04 -0700

    Implement get all posts api

--- src/main/java/com/codingjx/blogservice/service/PostService.java
+++ src/main/java/com/codingjx/blogservice/service/PostService.java
@@ -2,6 +2,10 @@ package com.codingjx.blogservice.service;
 
 import com.codingjx.blogservice.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
 }
--- src/main/java/com/codingjx/blogservice/service/impl/PostServiceImpl.java
+++ src/main/java/com/codingjx/blogservice/service/impl/PostServiceImpl.java
@@ -7,6 +7,9 @@ import com.codingjx.blogservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
     private PostRepository postRepository;
@@ -18,19 +21,31 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+        PostDto postResponse = mapToDto(newPost);
+        return postResponse;
+    }
+
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+    }
+
+    private Post mapToEntity(PostDto postDto) {
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
+        return post;
+    }
 
-        Post newPost = postRepository.save(post);
-
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-
-        return postResponse;
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
     }
 }
