da175c11dc0b5131d2826545538013dcff8940ea
chiahsingchu <chiahsingtony@gmail.com>
2023-08-28 19:04:07 -0700

    Create comment entity, and the relationship between comment and post

---
+++ src/main/java/com/codingjx/blogservice/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.codingjx.blogservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String email;
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
