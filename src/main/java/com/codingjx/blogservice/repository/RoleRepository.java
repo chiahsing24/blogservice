6c30ade446db72e082632e2cbb8df37471f29d67
chiahsingchu <chiahsingtony@gmail.com>
2023-09-01 19:58:36 -0700

    Implement user and role repositories

---
+++ src/main/java/com/codingjx/blogservice/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.codingjx.blogservice.repository;
+
+import com.codingjx.blogservice.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
